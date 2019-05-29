659873d121278544fe1097b286541979dcec59c7
OokamiTheLord <kuba@axod.pl>
2019-05-29 02:42:08 +0200

    client powinien działać (wygląda na to, że przesyła dane w poprawnej postaci, ale jeszcze nie ma sprawnego servera żeby to sprawdzić)

--- 9/6/client.c
+++ 9/6/client.c
@@ -17,18 +17,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
 
 /* Generuje losowe iv
  *należy zwolnić później zaalokowaną pamięć poprzez free()
  */
-char* generate_iv()
+unsigned char* generate_iv()
 {
     srand(time(NULL));
-    char* iv = (char*) malloc(16*sizeof(char));
+    char* iv = (char*) malloc(16*sizeof(unsigned char));
     int i;
     for(i=0; i<16; i++)
     {
-        iv[i] = rand()%(CHAR_MAX+1);
+        iv[i] = rand()%(UCHAR_MAX+1);
     }
 
     return iv;
@@ -41,7 +44,9 @@ int main(int argc, char** argv) {
     struct          sockaddr_in remote_addr;/* Gniazdowa struktura adresowa. */
     socklen_t       addr_len;               /* Rozmiar struktury w bajtach. */
 
-    char            buff[256] = "Laboratorium PUS.";
+    char            message[] = "Laboratorium PUS.";
+    unsigned char   hmac_psk[] = "Klucz HMAC";
+    unsigned char   cbc_psk[] = "Klucz CBC";
 
 
     if (argc != 3) {
@@ -74,15 +79,80 @@ int main(int argc, char** argv) {
     remote_addr.sin_port = htons(atoi(argv[2])); /* Numer portu. */
     addr_len = sizeof(remote_addr); /* Rozmiar struktury adresowej w bajtach. */
 
+
+    unsigned char* iv = generate_iv();
+
+    ERR_load_crypto_strings();
+
+    unsigned char *hmac = HMAC(EVP_md5(), hmac_psk, strlen(hmac_psk), (unsigned char*)message, strlen(message), NULL, NULL);
+
+    char buff[256];
+    int buff_len = 0;
+    snprintf(buff, sizeof(buff), "%s\n%s", hmac, message);
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_init(ctx);
+    const EVP_CIPHER* cipher = EVP_aes_128_cbc();
+    retval = EVP_EncryptInit_ex(ctx, cipher, NULL, cbc_psk, iv);
+    if (!retval) 
+    {
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+    EVP_CIPHER_CTX_set_padding(ctx, 8);
+
+    buff_len = strlen(buff);
+
+    unsigned char ciphertext[256];
+    int ciphertext_len;
+
+    /* Szyfrowanie: */
+    retval = EVP_EncryptUpdate(ctx, ciphertext, &ciphertext_len, (unsigned char*)buff, buff_len);
+
+    if (!retval) 
+    {
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    retval = EVP_EncryptFinal_ex(ctx, ciphertext + ciphertext_len, &ciphertext_len);
+    if (!retval)
+    {
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+
+    EVP_CIPHER_CTX_cleanup(ctx);
+
+
     fprintf(stdout, "Sending message to %s.\n", argv[1]);
 
     /* sendto() wysyla dane na adres okreslony przez strukture 'remote_addr': */
     retval = sendto(
                  sockfd,
-                 buff, strlen(buff),
+                 ciphertext, strlen(ciphertext),
                  0,
                  (struct sockaddr*)&remote_addr, addr_len
              );
+
+    fprintf(stdout, "Sending iv to %s.\n", argv[1]);
+
+    /* sendto() wysyla dane na adres okreslony przez strukture 'remote_addr': */
+    retval = sendto(
+                 sockfd,
+                 iv, 16*sizeof(unsigned char),
+                 0,
+                 (struct sockaddr*)&remote_addr, addr_len
+             );
+
+
+
+    free(iv);
+    ERR_free_strings();
+
+
+
 
     if (retval == -1) {
         perror("sendto()");
